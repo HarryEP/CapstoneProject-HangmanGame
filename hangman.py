191ec4793c949e7502384593ed6036479d13b594
HarryP82 <44836842+HarryEP@users.noreply.github.com>
2023-05-30 16:17:15 +0100

    added the plan

---
+++ hangman.py
@@ -0,0 +1,7 @@
+'''
+The plan is to either import a set of words or have a list of 50/100 words to choose from at random.
+Then we will implement the game of hangman, using a system of lives for each wrong guess and showing all wrong guesses throughout
+while continuining to display the word so far.
+Then we can repeat this, and have a score for how many times the word is guessed vs not guessed.
+Also, we will make sure not to repeat words.
+'''
